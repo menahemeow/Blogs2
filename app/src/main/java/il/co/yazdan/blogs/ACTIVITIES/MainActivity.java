bac5c13a603cbe9eb878b8f92884a05ffd8ad8ce
yazdan <oriyaz123@gmail.com>
2023-09-14 10:59:44 +0300

    סתם ככה בא לי לעשות קומיט ראשון כי זין שלי על כולכם

--- app/src/main/java/il/co/yazdan/blogs/ACTIVITIES/MainActivity.java
+++ app/src/main/java/il/co/yazdan/blogs/ACTIVITIES/MainActivity.java
@@ -3,14 +3,31 @@ package il.co.yazdan.blogs.ACTIVITIES;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import il.co.yazdan.blogs.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
+    private Button btnAdd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        initliazeViews();
     }
+
+
+    private void initliazeViews() {
+        btnAddPost = findViewById(R.id. btnAddPost);
+
+        setListeners();
+
+    }
+
+    protected void setListeners() {
+        // Add listener to btnAddPost
+    }
+
 }
