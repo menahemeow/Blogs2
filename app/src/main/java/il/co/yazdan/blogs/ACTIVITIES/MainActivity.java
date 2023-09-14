6b2d028df05a8c8f63e4e7911aed5a7a989dda39
yazdan <oriyaz123@gmail.com>
2023-09-14 10:43:54 +0300

    סתם ככה בא לי לעשות קומיט ראשון כי זין שלי על כולכם

---
+++ app/src/main/java/il/co/yazdan/blogs/ACTIVITIES/MainActivity.java
@@ -0,0 +1,16 @@
+package il.co.yazdan.blogs.ACTIVITIES;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import il.co.yazdan.blogs.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
